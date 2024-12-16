9c322c8c55bbbc5b3680fa79e350bb0501bb7445
Ahmed Khairy Elsawy <86704972+elsawy2332@users.noreply.github.com>
2024-12-17 00:40:18 +0200

    Add files via upload

---
+++ On_demand_traffic_light_control/main.c
@@ -0,0 +1,21 @@
+/*
+ * project2.c
+ *
+ * Created: 8/11/2022 6:55:17 AM
+ * Author : Ahmed kahiry abdelbary
+ */ 
+
+#include "APP/APP.h"
+
+
+int main(void)
+{
+	APP_init();
+	
+    while (1)
+    {
+	    APP_start();
+		
+    }
+}
+
